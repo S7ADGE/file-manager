6bd8260add565a9f242c25714a4c2defa47aeec7
S7ADGE <sadge17sae@gmail.com>
2026-08-27 14:33:52 +0330

    feat: detect duplicate files using hash comparison

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def choose_action():
 
     print("\nWhat would you like to do?\n")
     print("\t1. Display the files and folders in the specified path")
-    print("\t2. Find and delete duplicate files")
+    print("\t2. Find duplicate files")
     print("\t3. Do both")
 
     choise = input("\nEnter your choice (1, 2, or 3) : ").replace(" ", "")
@@ -20,25 +20,25 @@ def choose_action():
 def action():
 
     choise = choose_action()
+    print("\n==================================== SHOW ====================================")
 
     if choise == "3":
 
-        show()
-        same_size()
+        show_content()
+        show_duplicate()
 
     else:
 
         if choise == "1":
 
-            show()
+            show_content()
 
         else:
 
-            same_size()
+            show_duplicate()
 
-def show():
+def show_content():
 
-    print("\n==================================== SHOW ====================================")
     print("\nDiratories : \n")
 
     for item in folder.iterdir():
@@ -67,24 +67,77 @@ def size():
 
             if item.stat().st_size in files_by_size:
 
-                files_by_size[item.stat().st_size].append(item.name)
+                files_by_size[item.stat().st_size].append(item)
 
             else:
 
-                files_by_size[item.stat().st_size] = [item.name]
+                files_by_size[item.stat().st_size] = [item]
 
     return files_by_size
 
 def same_size():
 
     duplicate_files = size()
-    print("\n========================== Potential Duplicates(size) ==========================\n")
+    same_list = []
     
     for files in duplicate_files.values():
 
         if len(files) > 1:
 
-            print(files)
+            same_list.append(files)
+
+    return same_list
+
+def same_hash():
+
+    size_list = same_size()
+    hash_dic = {}
+    
+    for group in size_list:
+
+        for item in group:
+
+            hash_object = hashlib.sha256()
+    
+            with open(item, "rb") as file:
+
+                while chunk := file.read(4096):
+
+                    hash_object.update(chunk)
+
+                hash_dic[item] = hash_object.hexdigest()
+
+    return hash_dic
+
+def duplicate():
+
+    same = same_hash()
+    hash_list = []
+    duplicate_list = []
+
+    for key, value in same.items():
+
+        if value in hash_list:
+
+            duplicate_list.append(key.name)
+
+        hash_list.append(value)
+
+    return duplicate_list
+
+def show_duplicate():
+
+    duplicate_file = duplicate()
+    print("\n", "-" * 77, "\n")
+    print("Duplicate Files : \n")
+
+    if duplicate_file == []:
+
+        print("No duplicate files were found in this path.")
+
+    for file in duplicate_file:
+
+        print("\t", file)
 
 path = input("Enter the folder path : ")
 folder = Path(path).expanduser()
